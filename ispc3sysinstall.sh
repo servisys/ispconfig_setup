cbd5d1ea061034495bcad629c0e10efea78dcf04
Matteo Temporini <temporini.matteo@gmail.com>
2015-04-15 00:17:25 +0200

    Generated roundcube nginx file through script
    Generated roundcube nginx file through script, and remove cp and pwd

--- ispc3sysinstall.sh
+++ ispc3sysinstall.sh
@@ -19,7 +19,6 @@
 # Global variables
 #---------------------------------------------------------------------
 CFG_HOSTNAME_FQDN=`hostname -f`;
-PWD=$(pwd);
 WT_BACKTITLE="ISPConfig 3 System Installer from Temporini Matteo"
 
 
@@ -500,7 +499,52 @@ InstallWebmail() {
 	  	sed -i "/Options +FollowSymLinks/a\\`echo -e '\n\r'`  DirectoryIndex index.php\\`echo -e '\n\r'`\\`echo -e '\n\r'`  <IfModule mod_php5.c>\\`echo -e '\n\r'`        AddType application/x-httpd-php .php\\`echo -e '\n\r'`\\`echo -e '\n\r'`        php_flag magic_quotes_gpc Off\\`echo -e '\n\r'`        php_flag track_vars On\\`echo -e '\n\r'`        php_flag register_globals Off\\`echo -e '\n\r'`        php_value include_path .:/usr/share/php\\`echo -e '\n\r'`  </IfModule>" /etc/roundcube/apache.conf
 	  	sed -i "s/\$rcmail_config\['default_host'\] = '';/\$rcmail_config\['default_host'\] = 'localhost';/" /etc/roundcube/main.inc.php
 	  else
-		cp -prv $PWD/conf/roundcube.conf.nginx /etc/nginx/roundcube.conf
+		echo "  location /roundcube {" > /etc/nginx/roundcube.conf
+		echo "          root /var/lib/;" >> /etc/nginx/roundcube.conf
+		echo "           index index.php index.html index.htm;" >> /etc/nginx/roundcube.conf
+		echo "           location ~ ^/roundcube/(.+\.php)\$ {" >> /etc/nginx/roundcube.conf
+		echo "                   try_files \$uri =404;" >> /etc/nginx/roundcube.conf
+		echo "                   root /var/lib/;" >> /etc/nginx/roundcube.conf
+		echo "                   fastcgi_param   QUERY_STRING            \$query_string;" >> /etc/nginx/roundcube.conf
+		echo "                   fastcgi_param   REQUEST_METHOD          \$request_method;" >> /etc/nginx/roundcube.conf
+		echo "                   fastcgi_param   CONTENT_TYPE            \$content_type;" >> /etc/nginx/roundcube.conf
+		echo "                   fastcgi_param   CONTENT_LENGTH          \$content_length;" >> /etc/nginx/roundcube.conf
+		echo "                   fastcgi_param   SCRIPT_FILENAME         \$request_filename;" >> /etc/nginx/roundcube.conf
+		echo "                   fastcgi_param   SCRIPT_NAME             \$fastcgi_script_name;" >> /etc/nginx/roundcube.conf
+		echo "                   fastcgi_param   REQUEST_URI             \$request_uri;" >> /etc/nginx/roundcube.conf
+		echo "                   fastcgi_param   DOCUMENT_URI            \$document_uri;" >> /etc/nginx/roundcube.conf
+		echo "                   fastcgi_param   DOCUMENT_ROOT           \$document_root;" >> /etc/nginx/roundcube.conf
+		echo "                   fastcgi_param   SERVER_PROTOCOL         \$server_protocol;" >> /etc/nginx/roundcube.conf
+		echo "                   fastcgi_param   GATEWAY_INTERFACE       CGI/1.1;" >> /etc/nginx/roundcube.conf
+		echo "                   fastcgi_param   SERVER_SOFTWARE         nginx/\$nginx_version;" >> /etc/nginx/roundcube.conf
+		echo "                   fastcgi_param   REMOTE_ADDR             \$remote_addr;" >> /etc/nginx/roundcube.conf
+		echo "                   fastcgi_param   REMOTE_PORT             \$remote_port;" >> /etc/nginx/roundcube.conf
+		echo "                   fastcgi_param   SERVER_ADDR             \$server_addr;" >> /etc/nginx/roundcube.conf
+		echo "                   fastcgi_param   SERVER_PORT             \$server_port;" >> /etc/nginx/roundcube.conf
+		echo "                   fastcgi_param   SERVER_NAME             \$server_name;" >> /etc/nginx/roundcube.conf
+		echo "                   fastcgi_param   HTTPS                   \$https;" >> /etc/nginx/roundcube.conf
+		echo "                   # PHP only, required if PHP was built with --enable-force-cgi-redirect" >> /etc/nginx/roundcube.conf
+		echo "                   fastcgi_param   REDIRECT_STATUS         200;" >> /etc/nginx/roundcube.conf
+		echo "                   # To access SquirrelMail, the default user (like www-data on Debian/Ubuntu) mu\$" >> /etc/nginx/roundcube.conf
+		echo "                   #fastcgi_pass 127.0.0.1:9000;" >> /etc/nginx/roundcube.conf
+		echo "                   fastcgi_pass unix:/var/run/php5-fpm.sock;" >> /etc/nginx/roundcube.conf
+		echo "                   fastcgi_index index.php;" >> /etc/nginx/roundcube.conf
+		echo "                   fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;" >> /etc/nginx/roundcube.conf
+		echo "                   fastcgi_buffer_size 128k;" >> /etc/nginx/roundcube.conf
+		echo "                   fastcgi_buffers 256 4k;" >> /etc/nginx/roundcube.conf
+		echo "                   fastcgi_busy_buffers_size 256k;" >> /etc/nginx/roundcube.conf
+		echo "                   fastcgi_temp_file_write_size 256k;" >> /etc/nginx/roundcube.conf
+		echo "           }" >> /etc/nginx/roundcube.conf
+		echo "           location ~* ^/roundcube/(.+\.(jpg|jpeg|gif|css|png|js|ico|html|xml|txt))\$ {" >> /etc/nginx/roundcube.conf
+		echo "                   root /var/lib/;" >> /etc/nginx/roundcube.conf
+		echo "           }" >> /etc/nginx/roundcube.conf
+		echo "           location ~* /.svn/ {" >> /etc/nginx/roundcube.conf
+		echo "                   deny all;" >> /etc/nginx/roundcube.conf
+		echo "           }" >> /etc/nginx/roundcube.conf
+		echo "           location ~* /README|INSTALL|LICENSE|SQL|bin|CHANGELOG\$ {" >> /etc/nginx/roundcube.conf
+		echo "                   deny all;" >> /etc/nginx/roundcube.conf
+		echo "           }" >> /etc/nginx/roundcube.conf
+		echo "          }" >> /etc/nginx/roundcube.conf
 		sed -i "1,5d" /etc/nginx/sites-enabled/default
 		sed -i "s/server {/server {\\`echo -e '\n\r'`include \/etc\/nginx\/roundcube.conf/" /etc/nginx/sites-enabled/default
 	  fi
