03ec7a0edba4ae43e8f8567b48dce7d62ae33aad
Temporini Matteo <temporini.matteo@gmail.com>
2015-04-15 12:36:28 +0200

    Added module for PreInstallCheck function

--- ispc3sysinstall.sh
+++ ispc3sysinstall.sh
@@ -29,11 +29,9 @@ NC='\033[0m' # No Color
 #Program Versions
 JKV="2.17"  #Jailkit Version -> Maybe this can be automated
 
+#Saving current directory
+PWD=$(pwd);
 
-#---------------------------------------------------------------------
-# Function: PreInstallCheck
-#    Do some pre-install checks
-#---------------------------------------------------------------------
 clear
 
 # Check if user is root
@@ -42,32 +40,7 @@ if [ $(id -u) != "0" ]; then
     exit 1
 fi
 
-PreInstallCheck() {
-  echo -n "Checking internet connection.."
-  ping -q -c 3 www.ispconfig.org > /dev/null 2>&1
-
-  if [ ! "$?" -eq 0 ]; then
-	echo -e "${red}ERROR: Couldn't reach www.ispconfig.org, please check your internet connection${NC}"
-	exit 1;
-  fi
-  
-  # Check source.list
-  contrib=$(cat /etc/apt/sources.list | grep contrib | grep -v "cdrom")
-  nonfree=$(cat /etc/apt/sources.list | grep non-free | grep -v "cdrom")
-  if [ -z "$contrib" ]; then
-        if [ -z "$nonfree" ]; then
-                sed -i 's/main/main contrib non-free/' /etc/apt/sources.list;
-        else
-                sed -i 's/main/main contrib/' /etc/apt/sources.list;
-        fi
-  else
-        if [ -z "$nonfree" ]; then
-                sed -i 's/main/main non-free/' /etc/apt/sources.list;
-        fi
-  fi
-  echo -e "${green}OK${NC}\n"
-}
-
+include $PWD/debian/modules/preinstallcheck.sh
 
 #---------------------------------------------------------------------
 # Function: AskQuestions
