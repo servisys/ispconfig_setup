5192076fdc5c38adf08e81744915db2cedcb7f6f
gOOvER <info@goover.de>
2015-04-17 09:26:00 +0200

    Small Fix

--- debian/modules/preinstallcheck.sh
+++ debian/modules/preinstallcheck.sh
@@ -54,7 +54,7 @@ fi
 echo "Your Distro is:" --->>> $DISTRO
 read -p "Is this correct? " -n 1 -r
 echo    # (optional) move to a new line
-if [[ ! $REPLY =~ ^[Nn]$ ]]
+if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
     exit 1
 fi
