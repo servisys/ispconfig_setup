2887ece93bc02eedc514b99da771440956c3e054
Matteo Temporini <temporini.matteo@gmail.com>
2015-04-19 23:47:26 +0200

    Fix preinstall check
    Added some check before starting installation

--- debian/modules/preinstallcheck.sh
+++ debian/modules/preinstallcheck.sh
@@ -3,12 +3,55 @@
 #    Do some pre-install checks
 #---------------------------------------------------------------------
 PreInstallCheck() {
+  # Check if user is root
+  if [ $(id -u) != "0" ]; then
+    echo "Error: You must be root to run this script, please use the root user to install the software."
+    exit 1
+  fi
+  
+  # Check connectivity
   echo -n "Checking internet connection.."
   ping -q -c 3 www.ispconfig.org > /dev/null 2>&1
 
   if [ ! "$?" -eq 0 ]; then
         echo -e "${red}ERROR: Couldn't reach www.ispconfig.org, please check your internet connection${NC}"
         exit 1;
+  fi
+  
+  # Detect currect Linux Version
+  # Debian Wheezy Detection
+  if command -v lsb_release &> /dev/null; then
+	if lsb_release -a 2> /dev/null | grep -iq "wheezy"; then
+		DISTRO=wheezy
+	fi
+  fi
+
+  # Debian Wheezy Detection
+  if command -v lsb_release &> /dev/null; then
+	if lsb_release -a 2> /dev/null | grep -iq "jessie"; then
+		DISTRO=jessie
+	fi
+  fi
+
+  # Ubuntu Detection
+  if command -v lsb_release &> /dev/null; then
+	if lsb_release -a 2> /dev/null | grep -iq "ubuntu"; then
+		DISTRO=ubuntu
+	fi
+  fi
+
+  echo "Your Distro is: " $DISTRO
+  read -p "Is this correct? (y/n)" -n 1 -r
+  echo    # (optional) move to a new line
+  if [[ ! $REPLY =~ ^[Yy]$ ]]
+  then
+    exit 1
+  fi
+
+  # Check for already isntalled ispconfig version
+  if [ -f /usr/local/ispconfig/interface/lib/config.inc.php ]; then
+    echo "ISPConfig is already installed, can't go on."
+	exit 1
   fi
   
   # Check source.list
@@ -28,33 +71,4 @@ PreInstallCheck() {
   echo -e "${green}OK${NC}\n"
 }
 
-# Detect currect Linux Version
 
-# Debian Wheezy Detection
-if command -v lsb_release &> /dev/null; then
-	if lsb_release -a 2> /dev/null | grep -iq "wheezy"; then
-		DISTRO=wheezy
-	fi
-fi
-
-# Debian Wheezy Detection
-if command -v lsb_release &> /dev/null; then
-	if lsb_release -a 2> /dev/null | grep -iq "jessie"; then
-		DISTRO=jessie
-	fi
-fi
-
-# Ubuntu Detection
-if command -v lsb_release &> /dev/null; then
-	if lsb_release -a 2> /dev/null | grep -iq "ubuntu"; then
-		DISTRO=ubuntu
-	fi
-fi
-
-echo "Your Distro is: " $DISTRO
-read -p "Is this correct? (y/n)" -n 1 -r
-echo    # (optional) move to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    exit 1
-fi
