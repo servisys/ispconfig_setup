c0f6c806ade1ff02568594a31af1670dfae04f40
gOOvER <info@goover.de>
2015-05-01 10:14:33 +0200

    Small Fix Part 1.000.000

--- distros/debian8/install_mysql.sh
+++ distros/debian8/install_mysql.sh
@@ -2,24 +2,23 @@
 # Function: InstallSQLServer
 #    Install and configure SQL Server
 #---------------------------------------------------------------------
-
 InstallSQLServer() {
   if [ $CFG_SQLSERVER == "MySQL" ]; then
-  echo -n "Installing MySQL... "
-  echo "mysql-server-5.5 mysql-server/root_password password $CFG_MYSQL_ROOT_PWD" | debconf-set-selections
-  echo "mysql-server-5.5 mysql-server/root_password_again password $CFG_MYSQL_ROOT_PWD" | debconf-set-selections
-  apt-get -y install mysql-client mysql-server > /dev/null 2>&1
-  sed -i 's/bind-address		= 127.0.0.1/#bind-address		= 127.0.0.1/' /etc/mysql/my.cnf
-  service mysql restart > /dev/null
-  echo -e "${green}done! ${NC}\n"
+    echo -n "Installing MySQL... "
+    echo "mysql-server-5.5 mysql-server/root_password password $CFG_MYSQL_ROOT_PWD" | debconf-set-selections
+    echo "mysql-server-5.5 mysql-server/root_password_again password $CFG_MYSQL_ROOT_PWD" | debconf-set-selections
+    apt-get -y install mysql-client mysql-server > /dev/null 2>&1
+    sed -i 's/bind-address		= 127.0.0.1/#bind-address		= 127.0.0.1/' /etc/mysql/my.cnf
+    service mysql restart > /dev/null
+    echo -e "${green}done! ${NC}\n"
   
   else
   
-  echo -n "Installing MariaDB... "
-  echo "mysql-server-5.5 mysql-server/root_password password $CFG_MYSQL_ROOT_PWD" | debconf-set-selections
-  echo "mysql-server-5.5 mysql-server/root_password_again password $CFG_MYSQL_ROOT_PWD" | debconf-set-selections
-  apt-get -y install mariadb-client mariadb-server > /dev/null 2>&1
-  sed -i 's/bind-address		= 127.0.0.1/#bind-address		= 127.0.0.1/' /etc/mysql/my.cnf
-  service mysql restart /dev/null 2>&1
-  echo -e "${green}done! ${NC}\n"
+    echo -n "Installing MariaDB... "
+    echo "mysql-server-5.5 mysql-server/root_password password $CFG_MYSQL_ROOT_PWD" | debconf-set-selections
+    echo "mysql-server-5.5 mysql-server/root_password_again password $CFG_MYSQL_ROOT_PWD" | debconf-set-selections
+    apt-get -y install mariadb-client mariadb-server > /dev/null 2>&1
+    sed -i 's/bind-address		= 127.0.0.1/#bind-address		= 127.0.0.1/' /etc/mysql/my.cnf
+    service mysql restart /dev/null 2>&1
+    echo -e "${green}done! ${NC}\n"
 }
