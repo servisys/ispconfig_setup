0eb4214de4ef51ba4912b1e0eae81830214f3403
gOOvER <info@goover.de>
2015-05-01 13:30:59 +0200

    Remi

--- distros/debian8/install_antivirus.sh
+++ distros/debian8/install_antivirus.sh
@@ -2,7 +2,6 @@
 # Function: InstallAntiVirus
 #    Install Amavisd, Spamassassin, ClamAV
 #---------------------------------------------------------------------
-
 InstallAntiVirus() {
   echo -n "Installing Anti-Virus utilities... (This take some time. Don't abort it ...) "
   apt-get install amavisd-new spamassassin clamav clamav-daemon zoo unzip bzip2 arj nomarch lzop cabextract apt-listchanges libnet-ldap-perl libauthen-sasl-perl clamav-docs daemon libio-string-perl libio-socket-ssl-perl libnet-ident-perl zip libnet-dns-perl rkhunter > /dev/null 2>&1
@@ -13,7 +12,8 @@ InstallAntiVirus() {
   echo -n "Disable Spamassassin ... "
   systemctl disable spamassassin
   echo -e " [${green}done${NC}]\n"
-  echo -n "Restarting ClanAV... "
+  echo -n "Updateing ClamAV. Please Wait ... "
+  #freshclam
   service clamav-daemon restart
   echo -e " [${green}done${NC}]\n"
 }
