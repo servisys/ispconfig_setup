24ebb9ef8b636771a227c208ab6b68fdbcb8fd66
Matteo Temporini <temporini.matteo@gmail.com>
2015-05-03 22:41:44 +0200

    Fix nginx packages
    Fix nginx packages, removed php5-ming php5-ps

--- distros/debian8/install_webserver.sh
+++ distros/debian8/install_webserver.sh
@@ -47,7 +47,7 @@ InstallWebServer() {
 	update-rc.d -f apache2 remove
 	apt-get -yqq install nginx > /dev/null 2>&1
 	service nginx start 
-	apt-get -yqq install php5-fpm php5-mysqlnd php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-memcached php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl memcached php-apc > /dev/null 2>&1
+	apt-get -yqq install php5-fpm php5-mysqlnd php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-memcached php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl memcached php-apc > /dev/null 2>&1
 	sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php5/fpm/php.ini
 	sed -i "s/;date.timezone =/date.timezone=\"Europe\/Rome\"/" /etc/php5/fpm/php.ini
 	#sed -i "s/#/;/" /etc/php5/conf.d/ming.ini
