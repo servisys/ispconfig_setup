235ac7614c91bbe591e73d75c49ec5b78171f25d
Temporini Matteo <temporini.matteo@gmail.com>
2015-05-27 10:19:58 +0200

    Fix phpmyadmin install process

--- distros/debian7/install_webserver.sh
+++ distros/debian7/install_webserver.sh
@@ -76,7 +76,7 @@ InstallWebServer() {
 		echo "Press ENTER to continue... "
 		read DUMMY
 		echo -n "Installing phpMyAdmin... "
-		apt-get -yqq install phpmyadmin > /dev/null 2>&1
+		apt-get -yqq install phpmyadmin
 	   	echo "With nginx phpmyadmin is accessibile at  http://$CFG_HOSTNAME_FQDN:8081/phpmyadmin or http://IP_ADDRESS:8081/phpmyadmin"
 		echo -e "[${green}DONE${NC}]\n"
 	fi
