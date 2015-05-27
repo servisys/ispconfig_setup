0a0fdabe6fdb4572a2c0b8ed29fff5d793219821
Temporini Matteo <temporini.matteo@gmail.com>
2015-05-27 11:14:48 +0200

    Added warning to edit config.inc.php for roundcube

--- install.sh
+++ install.sh
@@ -123,6 +123,9 @@ if [ -f /etc/debian_version ]; then
   echo -e "${green}Well done ISPConfig installed and configured correctly :D ${NC}"
   echo "No you can connect to your ISPConfig installation ad https://$CFG_HOSTNAME_FQDN:8080 or https://IP_ADDRESS:8080"
   echo "You can visit my GitHub profile at https://github.com/servisys/ispconfig_setup/"
+  if [ $CFG_WEBMAIL == "roundcube" ]; then
+	echo -e "${red}You had to edit user/pass /var/lib/roundcube/plugins/ispconfig3_account/config/config.inc.php of roudcube user, as the one you inserted in ISPconfig ${NC}"
+  fi
   if [ $CFG_WEBSERVER == "nginx" ]; then
   	echo "Phpmyadmin is accessibile at  http://$CFG_HOSTNAME_FQDN:8081/phpmyadmin or http://IP_ADDRESS:8081/phpmyadmin";
 	echo "Webmail is accessibile at  http://$CFG_HOSTNAME_FQDN:8081/webmail or http://IP_ADDRESS:8081/webmail";
