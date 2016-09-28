d5a0b31e4d738615104e9272ed32fb3fb956665b
Matteo Temporini <temporini.matteo@gmail.com>
2016-09-28 19:23:43 +0200

    Adding support for ispconfig 16.04 and 16.10

---
+++ distros/ubuntu-16.10/install_webmail.sh
@@ -0,0 +1,42 @@
+#---------------------------------------------------------------------
+# Function: InstallWebmail
+#    Install the chosen webmail client. Squirrelmail or Roundcube
+#---------------------------------------------------------------------
+InstallWebmail() {
+  echo -n "Installing webmail client ($CFG_WEBMAIL)... "
+  echo "dictionaries-common dictionaries-common/default-wordlist select american (American English)" | debconf-set-selections
+  apt-get -yqq install squirrelmail wamerican > /dev/null 2>&1
+  ln -s /etc/squirrelmail/apache.conf /etc/apache2/conf-enabled/squirrelmail.conf
+  sed -i 1d /etc/squirrelmail/apache.conf
+  sed -i '1iAlias /webmail /usr/share/squirrelmail' /etc/squirrelmail/apache.conf
+
+	case $CFG_MTA in
+		"courier")
+		  sed -i 's/$imap_server_type       = "other";/$imap_server_type       = "courier";/' /etc/squirrelmail/config.php
+		  sed -i 's/$optional_delimiter     = "detect";/$optional_delimiter     = ".";/' /etc/squirrelmail/config.php
+		  sed -i 's/$default_folder_prefix          = "";/$default_folder_prefix          = "INBOX.";/' /etc/squirrelmail/config.php
+		  sed -i 's/$trash_folder                   = "INBOX.Trash";/$trash_folder                   = "Trash";/' /etc/squirrelmail/config.php
+		  sed -i 's/$sent_folder                    = "INBOX.Sent";/$sent_folder                    = "Sent";/' /etc/squirrelmail/config.php
+		  sed -i 's/$draft_folder                   = "INBOX.Drafts";/$draft_folder                   = "Drafts";/' /etc/squirrelmail/config.php
+		  sed -i 's/$default_sub_of_inbox           = true;/$default_sub_of_inbox           = false;/' /etc/squirrelmail/config.php
+		  sed -i 's/$delete_folder                  = false;/$delete_folder                  = true;/' /etc/squirrelmail/config.php
+		  ;;
+		"dovecot")
+		  sed -i 's/$imap_server_type       = "other";/$imap_server_type       = "dovecot";/' /etc/squirrelmail/config.php
+		  sed -i 's/$trash_folder                   = "INBOX.Trash";/$trash_folder                   = "Trash";/' /etc/squirrelmail/config.php
+		  sed -i 's/$sent_folder                    = "INBOX.Sent";/$sent_folder                    = "Sent";/' /etc/squirrelmail/config.php
+		  sed -i 's/$draft_folder                   = "INBOX.Drafts";/$draft_folder                   = "Drafts";/' /etc/squirrelmail/config.php
+		  sed -i 's/$default_sub_of_inbox           = true;/$default_sub_of_inbox           = false;/' /etc/squirrelmail/config.php
+		  sed -i 's/$delete_folder                  = false;/$delete_folder                  = true;/' /etc/squirrelmail/config.php
+		  ;;
+	  esac
+  mkdir /var/lib/squirrelmail/tmp
+  chown www-data /var/lib/squirrelmail/tmp
+  if [ "$CFG_WEBSERVER" == "apache" ]; then
+	  service apache2 restart > /dev/null 2>&1
+  else
+	  service nginx restart > /dev/null 2>&1
+  fi
+  echo -e "[${green}DONE${NC}]\n"
+}
+
