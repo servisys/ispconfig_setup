9fecea2f2b3d03eb8b447ab56c4494ac0338eca9
Matteo Temporini <temporini.matteo@gmail.com>
2016-09-29 22:25:09 +0200

    Small Fix

--- distros/ubuntu-16.04/install_webserver.sh
+++ distros/ubuntu-16.04/install_webserver.sh
@@ -51,10 +51,10 @@ InstallWebServer() {
 	service apache2 restart > /dev/null 2>&1
 
 	echo -n "Installing Lets Encrypt... "	
-	mkdir /opt/certbot
-	cd /opt/certbot
-	wget https://dl.eff.org/certbot-auto
-	chmod a+x ./certbot-auto
+	mkdir /opt/certbot > /dev/null 2>&1
+	cd /opt/certbot > /dev/null 2>&1
+	wget https://dl.eff.org/certbot-auto  > /dev/null 2>&1
+	chmod a+x ./certbot-auto  > /dev/null 2>&1
 	echo "==========================================================================================="
 	echo "Attention: answer no to next Question Dialog"
 	echo "==========================================================================================="
