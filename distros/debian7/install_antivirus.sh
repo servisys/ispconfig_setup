c4385b80628fa932aeb31d61d17225d829df56c3
Matteo Temporini <temporini.matteo@gmail.com>
2016-10-12 09:42:48 +0200

    Update install_antivirus.sh

--- distros/debian7/install_antivirus.sh
+++ distros/debian7/install_antivirus.sh
@@ -4,7 +4,7 @@
 #---------------------------------------------------------------------
 InstallAntiVirus() {
   echo -n "Installing Anti-Virus utilities... "
-  apt-get -y install amavisd-new spamassassin clamav clamav-daemon zoo unzip bzip2 arj nomarch lzop cabextract apt-listchanges libnet-ldap-perl libauthen-sasl-perl clamav-docs daemon libio-string-perl libio-socket-ssl-perl libnet-ident-perl zip libnet-dns-perl rkhunter > /dev/null 2>&1
+  apt-get -y install amavisd-new spamassassin clamav clamav-daemon zoo unzip bzip2 arj nomarch lzop cabextract apt-listchanges libnet-ldap-perl libauthen-sasl-perl clamav-docs daemon libio-string-perl libio-socket-ssl-perl libnet-ident-perl zip libnet-dns-perl rkhunter unrar-free p7zip rpm2cpio tnef > /dev/null 2>&1
   echo -n "Stopping Spamassassin ... "
   service spamassassin stop
   echo -e " [${green}done${NC}]\n"
