e66057451e2da2c2512efd0059a0cc6ff5de45a1
Matteo Temporini <temporini.matteo@gmail.com>
2016-10-12 09:42:13 +0200

    Update install_antivirus.sh

--- distros/debian8/install_antivirus.sh
+++ distros/debian8/install_antivirus.sh
@@ -4,7 +4,7 @@
 #---------------------------------------------------------------------
 InstallAntiVirus() {
   echo -n "Installing Anti-Virus utilities... (This take some time. Don't abort it ...) "
-  apt-get -yqq install amavisd-new spamassassin clamav clamav-daemon zoo unzip bzip2 arj nomarch lzop cabextract apt-listchanges libnet-ldap-perl libauthen-sasl-perl clamav-docs daemon libio-string-perl libio-socket-ssl-perl libnet-ident-perl zip libnet-dns-perl rkhunter systemd > /dev/null 2>&1
+  apt-get -yqq install amavisd-new spamassassin clamav clamav-daemon zoo unzip bzip2 arj nomarch lzop cabextract apt-listchanges libnet-ldap-perl libauthen-sasl-perl clamav-docs daemon libio-string-perl libio-socket-ssl-perl libnet-ident-perl zip libnet-dns-perl rkhunter systemd unrar-free p7zip rpm2cpio tnef > /dev/null 2>&1
   sed -i "s/AllowSupplementaryGroups false/AllowSupplementaryGroups true/" /etc/clamav/clamd.conf
   echo -n "Stopping Spamassassin ... "
   service spamassassin stop
