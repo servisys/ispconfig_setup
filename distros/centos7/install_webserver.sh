85283432491f3d44ca67b19d9b702abbd378c3db
Matteo Temporini <temporini.matteo@gmail.com>
2016-11-04 14:45:30 +0100

    Adding L'et's Encrypt support for Centos 7
    Adding L'et's Encrypt support for Centos 7

--- distros/centos7/install_webserver.sh
+++ distros/centos7/install_webserver.sh
@@ -87,6 +87,9 @@ InstallWebServer() {
     sed -i "s/Require ip 127.0.0.1/#Require ip 127.0.0.1/" /etc/httpd/conf.d/phpMyAdmin.conf
     sed -i '0,/Require ip ::1/ s/Require ip ::1/#Require ip ::1\n       Require all granted/' /etc/httpd/conf.d/phpMyAdmin.conf
 	sed -i "s/'cookie'/'http'/" /etc/phpMyAdmin/config.inc.php
+	echo "Installing Let's Encrypt... "
+	yum -y install letsencrypt > /dev/null 2>&1
+	echo -e "${green}done!${NC}\n"
 	systemctl enable  httpd.service
     systemctl restart  httpd.service
 	echo -e "${green}done! ${NC}\n"
