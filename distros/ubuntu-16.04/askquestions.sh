20d05bd5336f6e9eea044e41fa48784dffdfa45a
Matteo Temporini <temporini.matteo@gmail.com>
2017-04-02 10:37:52 +0200

    Update askquestions.sh

--- distros/ubuntu-16.04/askquestions.sh
+++ distros/ubuntu-16.04/askquestions.sh
@@ -13,7 +13,7 @@ AskQuestions() {
 		  
 	  while [ "x$CFG_MYSQL_ROOT_PWD" == "x" ]
 	  do
-				CFG_MYSQL_ROOT_PWD=$(whiptail --title "MySQL" --backtitle "$WT_BACKTITLE" --inputbox "Please specify a root password" --nocancel 10 50 3>&1 1>&2 2>&3)
+		CFG_MYSQL_ROOT_PWD=$(whiptail --title "MySQL" --backtitle "$WT_BACKTITLE" --inputbox "Please specify a root password" --nocancel 10 50 3>&1 1>&2 2>&3)
 	  done
 
 	  while [ "x$CFG_WEBSERVER" == "x" ]
@@ -22,38 +22,38 @@ AskQuestions() {
           done
 	
 	  while [ "x$CFG_XCACHE" == "x" ]
-		  do
-				CFG_XCACHE=$(whiptail --title "Install XCache" --backtitle "$WT_BACKTITLE" --nocancel --radiolist "You want to install XCache during install? ATTENTION: If XCache is installed, Ioncube Loaders will not work !!" 20 50 2 "yes" "(default)" ON "no" "" OFF 3>&1 1>&2 2>&3)
+	  do
+		CFG_XCACHE=$(whiptail --title "Install XCache" --backtitle "$WT_BACKTITLE" --nocancel --radiolist "You want to install XCache during install? ATTENTION: If XCache is installed, Ioncube Loaders will not work !!" 20 50 2 "yes" "(default)" ON "no" "" OFF 3>&1 1>&2 2>&3)
 	  done
 	
 	  while [ "x$CFG_PHPMYADMIN" == "x" ]
-		  do
-				CFG_PHPMYADMIN=$(whiptail --title "Install phpMyAdmin" --backtitle "$WT_BACKTITLE" --nocancel --radiolist "You want to install phpMyAdmin during install?" 10 50 2 "yes" "(default)" ON "no" "" OFF 3>&1 1>&2 2>&3)
+ 	  do
+		CFG_PHPMYADMIN=$(whiptail --title "Install phpMyAdmin" --backtitle "$WT_BACKTITLE" --nocancel --radiolist "You want to install phpMyAdmin during install?" 10 50 2 "yes" "(default)" ON "no" "" OFF 3>&1 1>&2 2>&3)
 	  done
 	
 	  while [ "x$CFG_MTA" == "x" ]
 	  do
-				CFG_MTA=$(whiptail --title "Mail Server" --backtitle "$WT_BACKTITLE" --nocancel --radiolist "Select mailserver type" 10 50 2 "dovecot" "(default)" ON "courier" "" OFF 3>&1 1>&2 2>&3)
+		CFG_MTA=$(whiptail --title "Mail Server" --backtitle "$WT_BACKTITLE" --nocancel --radiolist "Select mailserver type" 10 50 2 "dovecot" "(default)" ON "courier" "" OFF 3>&1 1>&2 2>&3)
 	  done
 	  
 	  while [ "x$CFG_WEBMAIL" == "x" ]
 	  do
-				CFG_WEBMAIL=$(whiptail --title "Webmail" --backtitle "$WT_BACKTITLE" --nocancel --radiolist "Select Webmail type" 10 50 2 "squirrelmail" "(default)" ON "roundcube" "" OFF 3>&1 1>&2 2>&3)
+		CFG_WEBMAIL=$(whiptail --title "Webmail" --backtitle "$WT_BACKTITLE" --nocancel --radiolist "Select Webmail type" 10 50 2 "squirrelmail" "(default)" ON "roundcube" "" OFF 3>&1 1>&2 2>&3)
 	  done
 	  
 	  while [ "x$CFG_HHVMINSTALL" == "x" ]
 	  do
-				CFG_HHVMINSTALL=$(whiptail --title "Install HHVM" --backtitle "$WT_BACKTITLE" --nocancel --radiolist "Do you want to install HHVM?" 10 50 2 "yes" "" OFF "no""(default)" ON 3>&1 1>&2 2>&3)
+		CFG_HHVMINSTALL=$(whiptail --title "Install HHVM" --backtitle "$WT_BACKTITLE" --nocancel --radiolist "Do you want to install HHVM?" 10 50 2 "yes" "" OFF "no""(default)" ON 3>&1 1>&2 2>&3)
 	  done
 	  
 	  while [ "x$CFG_METRONOM" == "x" ]
 	  do
-				CFG_METRONOM=$(whiptail --title "Install Metronom XMPP Server" --backtitle "$WT_BACKTITLE" --nocancel --radiolist "Do you want to install Metronom XMPP Server?" 10 50 2 "yes" "" OFF "no""(default)" ON 3>&1 1>&2 2>&3)
+		CFG_METRONOM=$(whiptail --title "Install Metronom XMPP Server" --backtitle "$WT_BACKTITLE" --nocancel --radiolist "Do you want to install Metronom XMPP Server?" 10 50 2 "yes" "" OFF "no""(default)" ON 3>&1 1>&2 2>&3)
 	  done
 	  
 	  while [ "x$CFG_AVUPDATE" == "x" ]
 	  do
-				CFG_AVUPDATE=$(whiptail --title "Update Freshclam DB" --backtitle "$WT_BACKTITLE" --nocancel --radiolist "You want to update Antivirus Database during install?" 10 50 2 "yes" "(default)" ON "no" "" OFF 3>&1 1>&2 2>&3)
+		CFG_AVUPDATE=$(whiptail --title "Update Freshclam DB" --backtitle "$WT_BACKTITLE" --nocancel --radiolist "You want to update Antivirus Database during install?" 10 50 2 "yes" "(default)" ON "no" "" OFF 3>&1 1>&2 2>&3)
 	  done
 	  
 	  if (whiptail --title "Quota" --backtitle "$WT_BACKTITLE" --yesno "Setup user quota?" 10 50) then
