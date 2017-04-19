b9940b6cbe543f247d90d4a9766ac921e12cfc68
René Bigler <raziel@dunkelweiss.li>
2017-04-19 21:23:47 +0200

    Add missing '>' to mysql start command

--- distros/debian8/install_mysql.sh
+++ distros/debian8/install_mysql.sh
@@ -19,7 +19,7 @@ InstallSQLServer() {
     echo "mysql-server-5.5 mysql-server/root_password_again password $CFG_MYSQL_ROOT_PWD" | debconf-set-selections
     apt-get -y install mariadb-client mariadb-server > /dev/null 2>&1
     sed -i 's/bind-address		= 127.0.0.1/#bind-address		= 127.0.0.1/' /etc/mysql/my.cnf
-    service mysql restart /dev/null 2>&1
+    service mysql restart > /dev/null 2>&1
     echo -e "[${green}DONE${NC}]\n"
   fi	
 }
