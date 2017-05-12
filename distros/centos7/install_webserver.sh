b3f1865d627f1827cc993a63de4b80d4f31d6011
Richard Siddall <richard.siddall@elirion.net>
2017-05-12 16:51:27 -0400

    Taking out the old, ineffective attempt to install Let's Encrypt.

--- distros/centos7/install_webserver.sh
+++ distros/centos7/install_webserver.sh
@@ -87,9 +87,6 @@ InstallWebServer() {
     sed -i "s/Require ip 127.0.0.1/#Require ip 127.0.0.1/" /etc/httpd/conf.d/phpMyAdmin.conf
     sed -i '0,/Require ip ::1/ s/Require ip ::1/#Require ip ::1\n       Require all granted/' /etc/httpd/conf.d/phpMyAdmin.conf
 	sed -i "s/'cookie'/'http'/" /etc/phpMyAdmin/config.inc.php
-	echo "Installing Let's Encrypt... "
-	yum -y install letsencrypt > /dev/null 2>&1
-	echo -e "${green}done!${NC}\n"
 	systemctl enable  httpd.service
     systemctl restart  httpd.service
 	echo -e "${green}done! ${NC}\n"
@@ -100,10 +97,8 @@ InstallWebServer() {
 
   echo -e "${green}done! ${NC}\n"
 
-  echo -n "Installing Lets Encrypt... "	
+  echo -n "Installing Let's Encrypt... "	
   yum -y install certbot
-  systemctl enable certbot.service
-  systemctl start certbot.service
 
   echo -e "${green}done! ${NC}\n"
 }
