b601feced3f2f75fe8ca397665679474a7a973d1
Richard Siddall <richard.siddall@elirion.net>
2017-05-12 15:51:46 -0400

    First attempt at installing Metronome from GitHub, as in Debian 8.

---
+++ distros/centos7/install_metronom.sh
@@ -0,0 +1,18 @@
+#---------------------------------------------------------------------
+# Function: InstallMetronom
+#    Install Metronom Server
+#---------------------------------------------------------------------
+InstallMetronom() {
+  echo -n "Installing Metronome...";
+  yum -y install git lua lua-devel lua-filesystem libidn-devel openssl-devel lua-bitop lua-socket lua-sec luarocks
+  luarocks install lpc
+  adduser --no-create-home --shell /sbin/nologin --comment 'Metronome' metronome
+  cd /opt; git clone https://github.com/maranda/metronome.git metronome
+  cd ./metronome; ./configure --ostype=centos --prefix=/usr
+  make
+  make install
+  pushd /etc/metronome/certs && make localhost.key && make localhost.csr && make localhost.cert && chmod 0400 localhost.key && chown metronome localhost.key
+  popd
+  /bin/rm -rf metronome
+  echo -e "[${green}DONE${NC}]\n"
+}
