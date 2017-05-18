8ca078a1225996e6c4669b16b5ebe3638d195521
Richard Siddall <richard.siddall@elirion.net>
2017-05-18 10:24:54 -0400

    Incorrect variable name for mailman site-wide list owner's email address.

--- distros/centos7/askquestions.sh
+++ distros/centos7/askquestions.sh
@@ -42,7 +42,7 @@ AskQuestions() {
 	  if (whiptail --title "Mailman" --backtitle "$WT_BACKTITLE" --yesno "Would you like to install Mailman?" 10 50) then
 		CFG_MAILMAN=yes
 		MMSITEPASS=$(whiptail --title "Mailman Site Password" --backtitle "$WT_BACKTITLE" --inputbox "Please specify the Mailman site password" --nocancel 10 50 3>&1 1>&2 2>&3)
-		MMISTOWNER=$(whiptail --title "Mailman Site List Owner" --backtitle "$WT_BACKTITLE" --inputbox "Please specify the Mailman site list owner" --nocancel 10 50 3>&1 1>&2 2>&3)
+		MMLISTOWNER=$(whiptail --title "Mailman Site List Owner" --backtitle "$WT_BACKTITLE" --inputbox "Please specify the Mailman site list owner" --nocancel 10 50 3>&1 1>&2 2>&3)
 		MMLISTPASS=$(whiptail --title "Mailman Site List Password" --backtitle "$WT_BACKTITLE" --inputbox "Please specify the Mailman site list password" --nocancel 10 50 3>&1 1>&2 2>&3)
 	  else
 		CFG_MAILMAN=no
