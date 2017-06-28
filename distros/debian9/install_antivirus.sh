91ef7b22ae2df15017e96158dc008b83c52e4de2
Matteo Temporini <info@servisys.it>
2017-06-28 20:08:13 +0200

    Fix for Debian 9
    Fix for Debian 9

--- distros/debian9/install_antivirus.sh
+++ distros/debian9/install_antivirus.sh
@@ -6,6 +6,11 @@ InstallAntiVirus() {
   echo -n "Installing Anti-Virus utilities... (This take some time. Don't abort it ...) "
   apt-get -yqq install amavisd-new spamassassin clamav clamav-daemon zoo unzip bzip2 arj nomarch lzop cabextract apt-listchanges libnet-ldap-perl libauthen-sasl-perl clamav-docs daemon libio-string-perl libio-socket-ssl-perl libnet-ident-perl zip libnet-dns-perl postgrey rkhunter systemd unrar-free p7zip rpm2cpio tnef > /dev/null 2>&1
   sed -i "s/AllowSupplementaryGroups false/AllowSupplementaryGroups true/" /etc/clamav/clamd.conf
+  echo "use strict;" > /etc/amavis/conf.d/05-node_id
+  echo "chomp(\$myhostname = \`hostname --fqdn\`);" >> /etc/amavis/conf.d/05-node_id
+  echo "\$myhostname = \"$CFG_HOSTNAME_FQDN\";" >> /etc/amavis/conf.d/05-node_id
+  echo "1;" >> /etc/amavis/conf.d/05-node_id
+  echo $CFG_HOSTNAME_FQDN > /etc/mailname
   echo -n "Stopping Spamassassin ... "
   service spamassassin stop
   echo -e "[${green}DONE${NC}]\n"
--- distros/debian9/install_webserver.sh
+++ distros/debian9/install_webserver.sh
@@ -17,7 +17,7 @@ InstallWebServer() {
 	apt-get -yqq install php7.0 php7.0-common php7.0-gd php7.0-mysql php7.0-imap php7.0-cli php7.0-cgi php-pear php7.0-mcrypt php7.0-curl php7.0-intl php7.0-pspell php7.0-recode php7.0-sqlite3 php7.0-tidy php7.0-xmlrpc php7.0-xsl php7.0-zip php7.0-mbstring php7.0-imap php7.0-mcrypt php7.0-snmp php7.0-xmlrpc php7.0-xsl  > /dev/null 2>&1
 	echo -e "[${green}DONE${NC}]\n"
 	echo -n "Installing PHP-FPM"
-	apt-get -yqq php7.0-fpm
+	apt-get -yqq install php7.0-fpm
 	a2enmod actions > /dev/null 2>&1 
 	a2enmod proxy_fcgi > /dev/null 2>&1 
 	a2enmod alias > /dev/null 2>&1 
