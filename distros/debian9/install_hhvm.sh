98203563c54d6b92acf096bf83bb11d6bfbaa6a5
gOOvER <info@goover.de>
2017-11-25 10:51:17 +0100

    added dirmngr, els keyimport end in error

--- distros/debian9/install_hhvm.sh
+++ distros/debian9/install_hhvm.sh
@@ -1,7 +1,7 @@
 InstallHHVM() {
   if [ $CFG_SETUP_WEB = "yes" ]; then
     echo -e "Installing HHVM"
-    apt-get install -yqq apt-transport-https software-properties-common
+    apt-get install -yqq apt-transport-https software-properties-common dirmngr
     apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xB4112585D386EB94
     add-apt-repository https://dl.hhvm.com/debian
     apt-get update
