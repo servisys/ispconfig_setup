37c0ac05d2b0d11fb1e0df3359628b81af4d7efc
Jonathan Hult <jonathan@jonathanhult.com>
2018-02-12 10:27:05 -0500

    Fix PHP path

--- distros/ubuntu-16.04/install_webserver.sh
+++ distros/ubuntu-16.04/install_webserver.sh
@@ -58,8 +58,8 @@ InstallWebServer() {
 	echo -n "Installing PHP and Modules... "
 	apt-get -yqq install php7.0-opcache php7.0-fpm php7.0 php7.0-common php7.0-gd php7.0-mysql php7.0-imap php7.0-cli php7.0-cgi php-pear php-auth php7.0-mcrypt mcrypt imagemagick libruby php7.0-curl php7.0-intl php7.0-pspell php7.0-recode php7.0-sqlite3 php7.0-tidy php7.0-xmlrpc php7.0-xsl memcached php-memcache php-imagick php-gettext php7.0-zip php7.0-mbstring php7.0-opcache php-apcu > /dev/null 2>&1
 	
-	sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php7.0/fpm/php.ini
-	sed -i "s/;date.timezone =/date.timezone=\"Europe\/Rome\"/" /etc/php7.0/fpm/php.ini
+	sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.0/fpm/php.ini
+	sed -i "s/;date.timezone =/date.timezone=\"Europe\/Rome\"/" /etc/php/7.0/fpm/php.ini
 	service php7.0-fpm reload
 	echo -e "[${green}DONE${NC}]\n"
 	
