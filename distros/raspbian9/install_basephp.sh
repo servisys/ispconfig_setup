db1b5980c38aa67a5e2241121ad4c4b2234c8f11
Teal Dulcet <tdulcet@pdx.edu>
2018-08-17 18:05:19 -0700

    Added suppport for Raspbian.

--- distros/raspbian9/install_basephp.sh
+++
@@ -1,9 +0,0 @@
-#---------------------------------------------------------------------
-# Function: InstallBasePhp Debian 8
-#    Install Basic php need to run ispconfig
-#---------------------------------------------------------------------
-InstallBasePhp(){
-  echo -n "Installing basic php modules for ispconfig..."
-  apt-get -yqq install php5-cli php5-mysql php5-mcrypt mcrypt > /dev/null 2>&1
-  echo -e "[${green}DONE${NC}]\n"
-}
