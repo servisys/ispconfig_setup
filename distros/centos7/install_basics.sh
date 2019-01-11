28a864cbf98d77ba19a8158e7b848e570a2b3995
BigLorD <FuryTr@yandex.com>
2019-01-11 14:46:26 +0300

    Update install_basics.sh

--- distros/centos7/install_basics.sh
+++ distros/centos7/install_basics.sh
@@ -28,7 +28,7 @@ InstallBasics() {
 
   #ref https://www.howtodojo.com/2017/10/install-git-centos-7/
   echo -n "Removing Git Old Version... "
-  yum remove -y git
+  hide_output yum remove -y git
   echo -n "Installing Git New Version... "
   yum install -y https://centos7.iuscommunity.org/ius-release.rpm
   yum install -y git2u
