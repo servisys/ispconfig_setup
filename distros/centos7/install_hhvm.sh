9ed2390368aa321425c409912c5846c57e978291
BigLorD <FuryTr@yandex.com>
2020-03-12 15:27:16 +0300

    Update install_hhvm.sh

--- distros/centos7/install_hhvm.sh
+++ distros/centos7/install_hhvm.sh
@@ -1,7 +1,7 @@
- echo -n "Installing Hhvm (nginx)... "
- yum install -y git zeromq-devel
- 
- yum install -y cpp gcc-c++ cmake psmisc {binutils,boost,jemalloc,numactl}-devel \
+ echo -n "Installing HHVM HipHop Virtual Machine (FCGI)... "
+ yum -y install -y epel-release git zeromq-devel
+
+ yum -y install cpp gcc-c++ cmake psmisc {binutils,boost,jemalloc,numactl}-devel \
  {ImageMagick,sqlite,tbb,bzip2,openldap,readline,elfutils-libelf,gmp,lz4,pcre}-devel \
  lib{xslt,event,yaml,vpx,png,zip,icu,mcrypt,memcached,cap,dwarf}-devel \
  {unixODBC,expat,mariadb}-devel lib{edit,curl,xml2,xslt}-devel \
