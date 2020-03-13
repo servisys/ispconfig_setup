4fecff4b5d8319315364086faa63584037f2eaf2
BigLorD <FuryTr@yandex.com>
2020-03-13 10:36:49 +0300

    Update askquestions.sh

--- distros/centos7/askquestions.sh
+++ distros/centos7/askquestions.sh
@@ -34,9 +34,9 @@ AskQuestions() {
 
 	if [[ ! "$CFG_HHVM" =~ $RE ]]; then
 	if (whiptail --title "HHVM" --backtitle "$WT_BACKTITLE" --nocancel --radiolist "Do you want to install HHVM (Hip Hop Virtual Machine) as PHP engine?" 10 50 2 "no" "(default)" ON "yes" "" OFF 3>&1 1>&2 2>&3) then
-			CFG_HHVM=n
+			CFG_HHVM=no
 		else
-			CFG_HHVM=y
+			CFG_HHVM=yes
 		fi
 	fi
 
