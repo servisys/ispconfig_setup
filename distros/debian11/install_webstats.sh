b901347e6a324e36a18149ff17f3a5f5b938a06b
Messi89 <messi89amine@gmail.com>
2021-12-03 22:34:07 +0100

    delete webalizer package

--- distros/debian11/install_webstats.sh
+++ distros/debian11/install_webstats.sh
@@ -5,7 +5,8 @@
 InstallWebStats() {
   echo -n "Installing Statistics (Webalizer and AWStats)... ";
   # apt_install vlogger webalizer awstats geoip-database libclass-dbi-mysql-perl libtimedate-perl
-  apt_install webalizer awstats geoip-database libclass-dbi-mysql-perl libtimedate-perl
+  #apt_install webalizer awstats geoip-database libclass-dbi-mysql-perl libtimedate-perl
+  apt_install awstats geoip-database libclass-dbi-mysql-perl libtimedate-perl
   sed -i 's/^/#/' /etc/cron.d/awstats
   echo -e "[${green}DONE${NC}]\n"
 }
